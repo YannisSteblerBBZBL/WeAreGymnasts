2cc892b7b9fb24260993330e459f821324c0fc59
YannisSteblerBBZBL <yannis.stebler@bbzbl-it.ch>
2024-06-20 09:43:45 +0200

    Delete Backend directory

--- Backend/src/main/java/com/bbzbl/yst/InformationGymnastics/repository/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bbzbl.yst.InformationGymnastics.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-import com.bbzbl.yst.InformationGymnastics.model.Blog;
-
-@Repository
-public interface BlogRepository extends MongoRepository<Blog, String> {}
